d3e631836a8bfd2f945656e71364b5db7ae774e6
chrislviv <73930081+chrislviv@users.noreply.github.com>
2026-08-12 11:36:58 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,24 @@ export default defineConfig({
     emptyOutDir: true,
     rollupOptions: {
       input: {
+        // Root
         main: 'index.html',
-        aboutEquipment: 'aboutEquipment.html',
-        aboutPhotographer: 'aboutPhotographer.html',
-        portfolio: 'portfolio.html',
-        portfolio2: 'portfolio2.html',
-        contact: 'contact.html',
-        expeditions: 'expeditions.html'
+        // Wildlife
+        wildlifeHome: 'wildlife/index.html',
+        aboutEquipment: 'wildlife/aboutEquipment.html',
+        aboutPhotographer: 'wildlife/aboutPhotographer.html',
+        portfolio: 'wildlife/portfolio.html',
+        portfolio2: 'wildlife/portfolio2.html',
+        contact: 'wildlife/contact.html',
+        expeditions: 'wildlife/expeditions.html',
+        // Cars
+        carsHome: 'cars/index.html',
+        carsAboutEquipment: 'cars/aboutEquipment.html',
+        carsAboutPhotographer: 'cars/aboutPhotographer.html',
+        carsPortfolio: 'cars/portfolio.html',
+        carsPortfolio2: 'cars/portfolio2.html',
+        carsContact: 'cars/contact.html',
+        carsExpeditions: 'cars/expeditions.html'
       }
     }
   }
